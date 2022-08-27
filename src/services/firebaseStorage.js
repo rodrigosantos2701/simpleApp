7d82dd98036198b37562fa4d286cf2f11d657148
Rodrigo.Santos <rodrigo.santos-ext@dalenys.com>
2022-08-27 17:31:59 -0300

    remove console log

--- src/services/firebaseStorage.js
+++ src/services/firebaseStorage.js
@@ -4,13 +4,6 @@ import 'react-native-get-random-values';
 
 export const saveOnStorage = async ({ isConfigData, userId, logo, id }) => {
 
-  console.log('-------------SAVE----------------');
-  console.log(isConfigData);
-  console.log(userId);
-  console.log(id);
-
-
-  console.log('------------------------------------');
 
   if (isConfigData) { id = 'config' }
   const storage = getStorage();
@@ -21,26 +14,16 @@ export const saveOnStorage = async ({ isConfigData, userId, logo, id }) => {
   const bytes = await img.blob()
   await uploadBytes(storageRef, bytes)
 
-  console.log('------------------------------------');
-  console.log('SALVO COM SUCESSSO');
-  console.log('------------------------------------');
 
 }
 
 export const deleteFromStorage = async (id) => {
-  console.log('------------DELETE----------------');
-  console.log(id);
-  console.log('------------------------------------');
-
   const storage = getStorage();
   const deleteRef = ref(storage, userId + '/' + id);
 
   // Delete the file
   deleteObject(deleteRef)
 
-  console.log('------------------------------------');
-  console.log('DELETED');
-  console.log('------------------------------------');
 
 
 }
