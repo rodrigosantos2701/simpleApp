edf248c23c3d81e443fe5ed9b017f492976be17d
Rodrigo.Santos <rodrigo.santos-ext@dalenys.com>
2022-09-01 12:02:59 -0300

    fix firebase service

--- src/services/firebaseStorage.js
+++ src/services/firebaseStorage.js
@@ -3,17 +3,17 @@ import 'react-native-get-random-values';
 
 
 export const saveOnStorage = async ({ isConfigData, userId, logo, id }) => {
-
-
-  if (isConfigData) { id = 'config' }
-  const storage = getStorage();
+  
+const storage = getStorage();
+console.log(logo)
+  if (isConfigData) { id = 'config' };
   const storageRef = ref(storage, userId + '/' + id);
+  if (logo == null) return;
 
   //Convert img         
   const img = await fetch(logo)
   const bytes = await img.blob()
   await uploadBytes(storageRef, bytes)
-
 
 }
 
